120f201f592da4c08c63011e545050d9da809c3a
Alejandro Cedillo Moctezuma <alejandrocmoc@hotmail.com>
2026-05-08 22:37:17 -0600

    Version Final Mejorada

--- src/entrada_web_asm_arm64.s
+++ src/entrada_web_asm_arm64.s
@@ -4,31 +4,20 @@
     .align  3
 
 // --- Cadenas literales ---
-_str_0:    .asciz "Ingrese los valores para la matriz 2x2:"
-_str_1:    .asciz "m[0][0]:"
-_str_2:    .asciz "m[0][1]:"
-_str_3:    .asciz "m[1][0]:"
-_str_4:    .asciz "m[1][1]:"
-_str_5:    .asciz "La suma de todos los elementos es:"
-_str_6:    .asciz "El promedio es:"
+_str_0:    .asciz "Cantidad:"
 _fmt_int:   .asciz "%lld\n"
 _fmt_str:   .asciz "%s\n"
 
 // --- Mapa de variables (stack offsets) ---
-//   m -> [x29, #-8]
-//   2x2 -> [x29, #-16]
-//   0,0 -> [x29, #-24]
-//   0,1 -> [x29, #-32]
-//   1,0 -> [x29, #-40]
-//   1,1 -> [x29, #-48]
-//   t1 -> [x29, #-56]
-//   t2 -> [x29, #-64]
-//   t3 -> [x29, #-72]
-//   t4 -> [x29, #-80]
-//   t5 -> [x29, #-88]
-//   t6 -> [x29, #-96]
-//   suma -> [x29, #-104]
-//   promedio -> [x29, #-112]
+//   n -> [x29, #-8]
+//   a -> [x29, #-16]
+//   b -> [x29, #-24]
+//   i -> [x29, #-32]
+//   L1 -> [x29, #-40]
+//   t1 -> [x29, #-48]
+//   L3 -> [x29, #-56]
+//   L2 -> [x29, #-64]
+//   temp -> [x29, #-72]
 
     .section __TEXT,__text
     .global _main
@@ -38,119 +27,82 @@ _main:
     // Prologo
     stp     x29, x30, [sp, #-16]!
     mov     x29, sp
-    sub     sp, sp, #128
+    sub     sp, sp, #96
 
-    // --- [0] NEWMAT m 2x2 - ---
-    // NEWMAT: operacion de matrices [pendiente]
-
-    // --- [1] WRITE "Ingrese los valores para la matriz 2x2:" - - ---
+    // --- [0] WRITE "Cantidad:" - - ---
     adrp    x0, _fmt_str@PAGE
     add     x0, x0, _fmt_str@PAGEOFF
     adrp    x1, _str_0@PAGE
     add     x1, x1, _str_0@PAGEOFF
     bl      _printf
 
-    // --- [2] WRITE "m[0][0]:" - - ---
-    adrp    x0, _fmt_str@PAGE
-    add     x0, x0, _fmt_str@PAGEOFF
-    adrp    x1, _str_1@PAGE
-    add     x1, x1, _str_1@PAGEOFF
+    // --- [1] READ - - n ---
+    // READ: lectura de entrada (requiere scanf)
+    // [pendiente: implementacion completa]
+
+    // --- [2] = 0 - a ---
+    mov     x9, #0
+    str     x9, [x29, #-16]
+
+    // --- [3] = 1 - b ---
+    mov     x9, #1
+    str     x9, [x29, #-24]
+
+    // --- [4] = 0 - i ---
+    mov     x9, #0
+    str     x9, [x29, #-32]
+
+    // --- [5] LABEL - - L1 ---
+L1:
+
+    // --- [6] < i n t1 ---
+    ldr     x9, [x29, #-32]
+    ldr     x10, [x29, #-8]
+    cmp     x9, x10
+    cset    x11, lt
+    str     x11, [x29, #-48]
+
+    // --- [7] IFT t1 - L3 ---
+    ldr     x9, [x29, #-48]
+    cbnz    x9, L3
+
+    // --- [8] GOTO - - L2 ---
+    b       L2
+
+    // --- [9] LABEL - - L3 ---
+L3:
+
+    // --- [10] WRITE a - - ---
+    adrp    x0, _fmt_int@PAGE
+    add     x0, x0, _fmt_int@PAGEOFF
+    ldr     x1, [x29, #-16]
     bl      _printf
 
-    // --- [3] MATREAD 0,0 - m ---
-    // Instruccion no traducida: MATREAD
-
-    // --- [4] WRITE "m[0][1]:" - - ---
-    adrp    x0, _fmt_str@PAGE
-    add     x0, x0, _fmt_str@PAGEOFF
-    adrp    x1, _str_2@PAGE
-    add     x1, x1, _str_2@PAGEOFF
-    bl      _printf
-
-    // --- [5] MATREAD 0,1 - m ---
-    // Instruccion no traducida: MATREAD
-
-    // --- [6] WRITE "m[1][0]:" - - ---
-    adrp    x0, _fmt_str@PAGE
-    add     x0, x0, _fmt_str@PAGEOFF
-    adrp    x1, _str_3@PAGE
-    add     x1, x1, _str_3@PAGEOFF
-    bl      _printf
-
-    // --- [7] MATREAD 1,0 - m ---
-    // Instruccion no traducida: MATREAD
-
-    // --- [8] WRITE "m[1][1]:" - - ---
-    adrp    x0, _fmt_str@PAGE
-    add     x0, x0, _fmt_str@PAGEOFF
-    adrp    x1, _str_4@PAGE
-    add     x1, x1, _str_4@PAGEOFF
-    bl      _printf
-
-    // --- [9] MATREAD 1,1 - m ---
-    // Instruccion no traducida: MATREAD
-
-    // --- [10] MATGET m 0,0 t1 ---
-    // MATGET: operacion de matrices [pendiente]
-
-    // --- [11] MATGET m 0,1 t2 ---
-    // MATGET: operacion de matrices [pendiente]
-
-    // --- [12] + t1 t2 t3 ---
-    ldr     x9, [x29, #-56]
-    ldr     x10, [x29, #-64]
+    // --- [11] + a b temp ---
+    ldr     x9, [x29, #-16]
+    ldr     x10, [x29, #-24]
     add     x11, x9, x10
     str     x11, [x29, #-72]
 
-    // --- [13] MATGET m 1,0 t4 ---
-    // MATGET: operacion de matrices [pendiente]
+    // --- [12] = b - a ---
+    ldr     x9, [x29, #-24]
+    str     x9, [x29, #-16]
 
-    // --- [14] + t3 t4 t5 ---
+    // --- [13] = temp - b ---
     ldr     x9, [x29, #-72]
-    ldr     x10, [x29, #-80]
+    str     x9, [x29, #-24]
+
+    // --- [14] + i 1 i ---
+    ldr     x9, [x29, #-32]
+    mov     x10, #1
     add     x11, x9, x10
-    str     x11, [x29, #-88]
+    str     x11, [x29, #-32]
 
-    // --- [15] MATGET m 1,1 t6 ---
-    // MATGET: operacion de matrices [pendiente]
+    // --- [15] GOTO - - L1 ---
+    b       L1
 
-    // --- [16] + t5 t6 suma ---
-    ldr     x9, [x29, #-88]
-    ldr     x10, [x29, #-96]
-    add     x11, x9, x10
-    str     x11, [x29, #-104]
-
-    // --- [17] WRITE "La suma de todos los elementos es:" - - ---
-    adrp    x0, _fmt_str@PAGE
-    add     x0, x0, _fmt_str@PAGEOFF
-    adrp    x1, _str_5@PAGE
-    add     x1, x1, _str_5@PAGEOFF
-    bl      _printf
-
-    // --- [18] WRITE suma - - ---
-    adrp    x0, _fmt_int@PAGE
-    add     x0, x0, _fmt_int@PAGEOFF
-    ldr     x1, [x29, #-104]
-    bl      _printf
-
-    // --- [19] / suma 4 promedio ---
-    ldr     x9, [x29, #-104]
-    mov     x10, #4
-    sdiv     x11, x9, x10
-    str     x11, [x29, #-112]
-
-    // --- [20] WRITE "El promedio es:" - - ---
-    adrp    x0, _fmt_str@PAGE
-    add     x0, x0, _fmt_str@PAGEOFF
-    adrp    x1, _str_6@PAGE
-    add     x1, x1, _str_6@PAGEOFF
-    bl      _printf
-
-    // --- [21] WRITE promedio - - ---
-    adrp    x0, _fmt_int@PAGE
-    add     x0, x0, _fmt_int@PAGEOFF
-    ldr     x1, [x29, #-112]
-    bl      _printf
+    // --- [16] LABEL - - L2 ---
+L2:
 
     // Epilogo
     mov     x0, #0
--- src/entrada_web_asm_x86.s
+++ src/entrada_web_asm_x86.s
@@ -3,31 +3,20 @@
     section .data
 
 ; --- Cadenas literales ---
-str_0:      db "Ingrese los valores para la matriz 2x2:", 0
-str_1:      db "m[0][0]:", 0
-str_2:      db "m[0][1]:", 0
-str_3:      db "m[1][0]:", 0
-str_4:      db "m[1][1]:", 0
-str_5:      db "La suma de todos los elementos es:", 0
-str_6:      db "El promedio es:", 0
+str_0:      db "Cantidad:", 0
 fmt_int:    db "%lld", 10, 0
 fmt_str:    db "%s", 10, 0
 
 ; --- Mapa de variables (stack offsets) ---
-;   m -> [rbp-8]
-;   2x2 -> [rbp-16]
-;   0,0 -> [rbp-24]
-;   0,1 -> [rbp-32]
-;   1,0 -> [rbp-40]
-;   1,1 -> [rbp-48]
-;   t1 -> [rbp-56]
-;   t2 -> [rbp-64]
-;   t3 -> [rbp-72]
-;   t4 -> [rbp-80]
-;   t5 -> [rbp-88]
-;   t6 -> [rbp-96]
-;   suma -> [rbp-104]
-;   promedio -> [rbp-112]
+;   n -> [rbp-8]
+;   a -> [rbp-16]
+;   b -> [rbp-24]
+;   i -> [rbp-32]
+;   L1 -> [rbp-40]
+;   t1 -> [rbp-48]
+;   L3 -> [rbp-56]
+;   L2 -> [rbp-64]
+;   temp -> [rbp-72]
 
     section .text
     global  main
@@ -37,113 +26,83 @@ main:
     ; Prologo
     push    rbp
     mov     rbp, rsp
-    sub     rsp, 128
+    sub     rsp, 96
 
-    ; --- [0] NEWMAT m 2x2 - ---
-    ; NEWMAT: operacion de matrices [pendiente]
-
-    ; --- [1] WRITE "Ingrese los valores para la matriz 2x2:" - - ---
+    ; --- [0] WRITE "Cantidad:" - - ---
     lea     rdi, [rel fmt_str]
     lea     rsi, [rel str_0]
     xor     eax, eax
     call    printf
 
-    ; --- [2] WRITE "m[0][0]:" - - ---
-    lea     rdi, [rel fmt_str]
-    lea     rsi, [rel str_1]
+    ; --- [1] READ - - n ---
+    ; READ: lectura de entrada (requiere scanf)
+    ; [pendiente: implementacion completa]
+
+    ; --- [2] = 0 - a ---
+    mov     rax, 0
+    mov     qword [rbp-16], rax
+
+    ; --- [3] = 1 - b ---
+    mov     rax, 1
+    mov     qword [rbp-24], rax
+
+    ; --- [4] = 0 - i ---
+    mov     rax, 0
+    mov     qword [rbp-32], rax
+
+    ; --- [5] LABEL - - L1 ---
+L1:
+
+    ; --- [6] < i n t1 ---
+    mov     rax, [rbp-32]
+    mov     rcx, [rbp-8]
+    cmp     rax, rcx
+    setl    al
+    movzx   rax, al
+    mov     qword [rbp-48], rax
+
+    ; --- [7] IFT t1 - L3 ---
+    mov     rax, [rbp-48]
+    cmp     rax, 0
+    jne     L3
+
+    ; --- [8] GOTO - - L2 ---
+    jmp     L2
+
+    ; --- [9] LABEL - - L3 ---
+L3:
+
+    ; --- [10] WRITE a - - ---
+    lea     rdi, [rel fmt_int]
+    mov     rsi, [rbp-16]
     xor     eax, eax
     call    printf
 
-    ; --- [3] MATREAD 0,0 - m ---
-    ; Instruccion no traducida: MATREAD
-
-    ; --- [4] WRITE "m[0][1]:" - - ---
-    lea     rdi, [rel fmt_str]
-    lea     rsi, [rel str_2]
-    xor     eax, eax
-    call    printf
-
-    ; --- [5] MATREAD 0,1 - m ---
-    ; Instruccion no traducida: MATREAD
-
-    ; --- [6] WRITE "m[1][0]:" - - ---
-    lea     rdi, [rel fmt_str]
-    lea     rsi, [rel str_3]
-    xor     eax, eax
-    call    printf
-
-    ; --- [7] MATREAD 1,0 - m ---
-    ; Instruccion no traducida: MATREAD
-
-    ; --- [8] WRITE "m[1][1]:" - - ---
-    lea     rdi, [rel fmt_str]
-    lea     rsi, [rel str_4]
-    xor     eax, eax
-    call    printf
-
-    ; --- [9] MATREAD 1,1 - m ---
-    ; Instruccion no traducida: MATREAD
-
-    ; --- [10] MATGET m 0,0 t1 ---
-    ; MATGET: operacion de matrices [pendiente]
-
-    ; --- [11] MATGET m 0,1 t2 ---
-    ; MATGET: operacion de matrices [pendiente]
-
-    ; --- [12] + t1 t2 t3 ---
-    mov     rax, [rbp-56]
-    mov     rcx, [rbp-64]
+    ; --- [11] + a b temp ---
+    mov     rax, [rbp-16]
+    mov     rcx, [rbp-24]
     add     rax, rcx
     mov     qword [rbp-72], rax
 
-    ; --- [13] MATGET m 1,0 t4 ---
-    ; MATGET: operacion de matrices [pendiente]
+    ; --- [12] = b - a ---
+    mov     rax, [rbp-24]
+    mov     qword [rbp-16], rax
 
-    ; --- [14] + t3 t4 t5 ---
+    ; --- [13] = temp - b ---
     mov     rax, [rbp-72]
-    mov     rcx, [rbp-80]
+    mov     qword [rbp-24], rax
+
+    ; --- [14] + i 1 i ---
+    mov     rax, [rbp-32]
+    mov     rcx, 1
     add     rax, rcx
-    mov     qword [rbp-88], rax
+    mov     qword [rbp-32], rax
 
-    ; --- [15] MATGET m 1,1 t6 ---
-    ; MATGET: operacion de matrices [pendiente]
+    ; --- [15] GOTO - - L1 ---
+    jmp     L1
 
-    ; --- [16] + t5 t6 suma ---
-    mov     rax, [rbp-88]
-    mov     rcx, [rbp-96]
-    add     rax, rcx
-    mov     qword [rbp-104], rax
-
-    ; --- [17] WRITE "La suma de todos los elementos es:" - - ---
-    lea     rdi, [rel fmt_str]
-    lea     rsi, [rel str_5]
-    xor     eax, eax
-    call    printf
-
-    ; --- [18] WRITE suma - - ---
-    lea     rdi, [rel fmt_int]
-    mov     rsi, [rbp-104]
-    xor     eax, eax
-    call    printf
-
-    ; --- [19] / suma 4 promedio ---
-    mov     rax, [rbp-104]
-    cqo
-    mov     rcx, 4
-    idiv    rcx
-    mov     qword [rbp-112], rax
-
-    ; --- [20] WRITE "El promedio es:" - - ---
-    lea     rdi, [rel fmt_str]
-    lea     rsi, [rel str_6]
-    xor     eax, eax
-    call    printf
-
-    ; --- [21] WRITE promedio - - ---
-    lea     rdi, [rel fmt_int]
-    mov     rsi, [rbp-112]
-    xor     eax, eax
-    call    printf
+    ; --- [16] LABEL - - L2 ---
+L2:
 
     ; Epilogo
     xor     eax, eax
